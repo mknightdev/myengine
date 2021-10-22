81c81bf6e747a42eda8da6b8a4d2cfcf586a5e1e
Michael Knight (s5211581) <s5211581@bournemouth.ac.uk>
2021-10-22 12:53:25 +0100

    Added glm

--- src/myengine/TriangleRenderer.h
+++ src/myengine/TriangleRenderer.h
@@ -1,5 +1,9 @@
 #include "Component.h"
 
+#include <GL/glew.h>
+#include <glm/glm.hpp>
+#include <vector>
+
 namespace myengine
 {
 	struct TriangleRenderer : Component
@@ -7,5 +11,20 @@ namespace myengine
 		void onInitialize();
 		void onDisplay();
 		void onTick();
+
+		// OpenGL Stuff
+		void add(glm::vec2 _value);
+		void add(glm::vec3 _value);
+		void add(glm::vec4 _value);
+
+		GLuint getId();
+
+		int getComponents();
+
+		private:
+			GLuint id;
+			std::vector<float> data;
+			bool dirty; // Trigger lazy uploading
+			int components;
 	};
 }
