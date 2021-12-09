85a39612da5cfb840823a854c41816e6f2fbec56
mknightdev <mknightdev@gmail.com>
2021-12-09 00:38:59 +0000

    PBR: Replaced Beckmann with Kelemen

--- src/demo/main.cpp
+++ src/demo/main.cpp
@@ -41,20 +41,20 @@ int main()
 	pbr->setBRDFShader("../resources/shaders/pbr/brdfVert.txt", "../resources/shaders/pbr/brdfFrag.txt");
 	pbr->setBackgroundShader("../resources/shaders/pbr/backgroundVert.txt", "../resources/shaders/pbr/backgroundFrag.txt");
 
-	// STEAM SPHERE
-	//=============
+	//// ENTITY & COMPONENTS
+	////====================
 	std::shared_ptr<Entity> pbrEntity2 = core->addEntity();
 	std::shared_ptr<PBR> pbr2 = pbrEntity2->addComponent<PBR>();
 
 	// MODEL & TEXTURES
 	//=================
-	pbr2->setMesh(core->getResourceManager()->load<Mesh>("../resources/models/pbrsphere/pbrsphere"));
-	pbr2->setAlbedo(core->getResourceManager()->load<Texture>("../resources/models/pbrsphere/pbrsphere_albedo"));
-	pbr2->setNormal(core->getResourceManager()->load<Texture>("../resources/models/pbrsphere/pbrsphere_normal"));
-	pbr2->setMetallic(core->getResourceManager()->load<Texture>("../resources/models/pbrsphere/pbrsphere_metallic"));
-	pbr2->setRoughness(core->getResourceManager()->load<Texture>("../resources/models/pbrsphere/pbrsphere_roughness"));
-	pbr2->setAo(core->getResourceManager()->load<Texture>("../resources/models/pbrsphere/pbrsphere_ao"));
-	pbr2->setEmissive(core->getResourceManager()->load<Texture>(""));
+	pbr2->setMesh(core->getResourceManager()->load<Mesh>("../resources/models/grenade/grenade"));
+	pbr2->setAlbedo(core->getResourceManager()->load<Texture>("../resources/models/grenade/grenade_albedo"));
+	pbr2->setNormal(core->getResourceManager()->load<Texture>("../resources/models/grenade/grenade_normal"));
+	pbr2->setMetallic(core->getResourceManager()->load<Texture>("../resources/models/grenade/grenade_metallic"));
+	pbr2->setRoughness(core->getResourceManager()->load<Texture>("../resources/models/grenade/grenade_roughness"));
+	pbr2->setAo(core->getResourceManager()->load<Texture>("../resources/models/grenade/grenade_ao"));
+	pbr2->setEmissive(core->getResourceManager()->load<Texture>("../resources/models/grenade/grenade_emissive"));
 
 	// SHADERS
 	//========
@@ -66,6 +66,32 @@ int main()
 	pbr2->setBackgroundShader("../resources/shaders/pbr/backgroundVert.txt", "../resources/shaders/pbr/backgroundFrag.txt");
 
 	pbr2->getTransform()->setPosition(vec3(-1, 0, -2.5f));
+
+	//// STEAM SPHERE
+	////=============
+	//std::shared_ptr<Entity> pbrEntity2 = core->addEntity();
+	//std::shared_ptr<PBR> pbr2 = pbrEntity2->addComponent<PBR>();
+
+	//// MODEL & TEXTURES
+	////=================
+	//pbr2->setMesh(core->getResourceManager()->load<Mesh>("../resources/models/pbrsphere/pbrsphere"));
+	//pbr2->setAlbedo(core->getResourceManager()->load<Texture>("../resources/models/pbrsphere/pbrsphere_albedo"));
+	//pbr2->setNormal(core->getResourceManager()->load<Texture>("../resources/models/pbrsphere/pbrsphere_normal"));
+	//pbr2->setMetallic(core->getResourceManager()->load<Texture>("../resources/models/pbrsphere/pbrsphere_metallic"));
+	//pbr2->setRoughness(core->getResourceManager()->load<Texture>("../resources/models/pbrsphere/pbrsphere_roughness"));
+	//pbr2->setAo(core->getResourceManager()->load<Texture>("../resources/models/pbrsphere/pbrsphere_ao"));
+	//pbr2->setEmissive(core->getResourceManager()->load<Texture>(""));
+
+	//// SHADERS
+	////========
+	//pbr2->setShader("../resources/shaders/pbr/pbrTexVert.txt", "../resources/shaders/pbr/pbrTexFrag2.txt");
+	//pbr2->setCubemapShader("../resources/shaders/pbr/cubemapVert.txt", "../resources/shaders/pbr/equirectangular_to_cubemapFrag.txt");
+	//pbr2->setIrradianceShader("../resources/shaders/pbr/cubemapVert.txt", "../resources/shaders/pbr/irradianceFrag.txt");
+	//pbr2->setPrefilterShader("../resources/shaders/pbr/cubemapVert.txt", "../resources/shaders/pbr/prefilterFrag.txt");
+	//pbr2->setBRDFShader("../resources/shaders/pbr/brdfVert.txt", "../resources/shaders/pbr/brdfFrag.txt");
+	//pbr2->setBackgroundShader("../resources/shaders/pbr/backgroundVert.txt", "../resources/shaders/pbr/backgroundFrag.txt");
+
+	//pbr2->getTransform()->setPosition(vec3(-1, 0, -2.5f));
 
 	//std::shared_ptr<AudioClip> s = core->getResourceManager()->load<AudioClip>("doorbell");
 
